a12e0d6dcf54d09f8d2858379aa24a8faf5a77cf
Stephen Leander <stephen.leander1@gmail.com>
2022-07-21 13:25:48 -0600

    Add signup, login & logout POST routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const user_controller = require('../controllers/userController');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/signup', user_controller.signup_post);
+router.post('/login', user_controller.login_post);
+router.post('/logout', user_controller.logout_post);
 
 module.exports = router;
