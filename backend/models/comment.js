8713c6b664961384754aa945064f6b48cd5f7546
Stephen Leander <stephen.leander1@gmail.com>
2022-07-22 16:52:42 -0600

    Rename author key

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -3,12 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-  authorName: { type: String, required: true },
+  author: { type: String, required: true },
   authorId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   timestamp: { type: Date, required: true },
   text: { type: String, required: true },
   puzzle: { type: Schema.Types.ObjectId, ref: 'Puzzle', required: true },
-  likes: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model('Comment', CommentSchema);
