f6ad13f0447c56f16f73d7ba20a535babd0177a9
Stephen Leander <stephen.leander1@gmail.com>
2022-07-22 11:20:12 -0600

    Add puzzle DELETE route

--- backend/routes/puzzles.js
+++ backend/routes/puzzles.js
@@ -7,5 +7,6 @@ const puzzle_controller = require('../controllers/puzzleController');
 router.get('/', puzzle_controller.puzzles_get);
 router.get('/:id', puzzle_controller.puzzle_get);
 router.post('/', upload.single('image'), puzzle_controller.puzzle_post);
+router.delete('/:id', puzzle_controller.puzzle_delete);
 
 module.exports = router;
